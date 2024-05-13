f845186338682d343f99fa62a114190273abd5f9
David <davidkhun2004@gmail.com>
2024-05-13 16:25:05 +0700

    Added all the basic operation of a calculator
    Added four function add, subtract, multiply and divide

---
+++ app.js
@@ -0,0 +1,29 @@
+// document.addEventListener('DOMContentLoaded',() => {
+//     const numbers = document.querySelectorAll('#numbers');
+//     numbers.forEach((num) =>{
+//         num.addEventListener('click', ()=>{
+//             console.log(num)
+//         })
+       
+        
+//     })
+//      console.log(numbers)
+// })
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2; 
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+console.log(multiply(2, 3))
