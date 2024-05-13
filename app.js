9efc7d7807e152b04b42c02e0ec597b476aecf3a
David <davidkhun2004@gmail.com>
2024-05-13 19:42:44 +0700

    Project status: Display number
    When click on the number it will display the number on the top

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-// document.addEventListener('DOMContentLoaded',() => {
-//     const numbers = document.querySelectorAll('#numbers');
-//     numbers.forEach((num) =>{
-//         num.addEventListener('click', ()=>{
-//             console.log(num)
-//         })
+document.addEventListener('DOMContentLoaded',() => {
+
+    const numbers = document.querySelectorAll('.num-button');
+    numbers.forEach((num) =>{
+        num.addEventListener('click', ()=>{
+            document.querySelector('#first-num').textContent = num.textContent
+        })
        
         
-//     })
-//      console.log(numbers)
-// })
+    })
+     
+})
 
 function add(num1, num2){
     return num1 + num2;
@@ -25,5 +26,3 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
-
-console.log(multiply(2, 3))
