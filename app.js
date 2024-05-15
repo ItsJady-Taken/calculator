73348e410626d763a0e8a1efc58f9f67a4fdf116
David <davidkhun2004@gmail.com>
2024-05-15 21:14:00 +0700

    Project Status: Complete all the required
    Project could use somefeatures but this is the best i can do for now

---
+++ app.js
@@ -0,0 +1,111 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('#all-button');
+let operator = null;
+let input = '';
+let previousInput = null;
+let result = '';
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const value = button.textContent
+
+        if(value >= '0' && value <= '9'){
+           handleNum(value) 
+        }
+        else if(value === 'C') {
+            clearAll();
+        }
+        else if(value === '←') {
+            backSpace();
+        }
+        else if(value === '=') {
+            operate();   
+        }
+        else if(value === '.') {
+            if (!input.includes('.')) {
+                input += '.';
+              }
+              display.textContent = input; // Update display after decimal input  
+        }
+        else {
+            handleOperator(value);
+        
+        }
+        // display the numbers and operator
+        if(operator != null) {
+            display.textContent = previousInput + operator + input // if the user press and operater
+        }
+        else if(value == '=') {
+            display.textContent = result.toString(); //convert Int into String (does not work when put in the above [else if])
+        }
+        else {
+            display.textContent = input;
+        }
+        
+    })
+})
+
+function clearAll() {
+    display.value = "";
+    input = '';
+    operator = null;
+    previousInput = null;
+}
+
+// alow multiple digit instead of just one
+function handleNum(value) {
+    if (input === '') {
+        input = value;
+    } 
+    else {
+        input += value;
+    }
+}
+
+// if the user press an operater, safe the input in a previousInput than reset input
+function handleOperator(value) {
+    operator = value;
+    if(previousInput === null) {
+        previousInput = input;
+        input = '';
+    }
+}
+
+// delete the last number and than update the display
+function backSpace() {
+    input = input.slice(0, -1);
+    display.textContent = input;
+}
+
+function operate() {
+    if(operator == null) {
+        clearAll()
+    }
+    // after each case reset everything
+    switch (operator) {
+        case '+':
+            result = parseFloat(previousInput) + parseFloat(input)
+            previousInput = '';
+            operator = null;
+            input = '';
+            break;
+        case '-':
+            result = parseFloat(previousInput) - parseFloat(input)
+            previousInput = '';
+            operator = null;
+            input = '';
+            break;
+        case '*':
+            result = parseFloat(previousInput) * parseFloat(input)
+            previousInput = '';
+            operator = null;
+            input = ''
+            break;
+        case '/':
+            result = parseFloat(previousInput) / parseFloat(input)
+            previousInput = '';
+            operator = null;
+            input = '';
+            break;               
+    }
+}
